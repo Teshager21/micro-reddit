da47e7fb12f8b06ce78c8aca4c066c971bbc7af7
Teshager <teshager8922@gmail.com>
2020-06-25 14:37:55 -0600

    adds email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 # User
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, presence: true, length: { minimim: 3, maximum: 254 }
-  validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
+  validates :first_name, :last_name, :email, presence: true, length: { minimum: 2, maximum: 12 }
+  validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }
   has_many :posts
 end
